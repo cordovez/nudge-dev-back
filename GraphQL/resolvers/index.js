e3d83a9f43c57d500703582d0cdc5105c08295b0
JC CorMan <cordovez@el-cordovez.com>
2022-08-11 14:13:44 +0200

    finished chapter 8 of tutorial

--- GraphQL/resolvers/index.js
+++ GraphQL/resolvers/index.js
@@ -2,79 +2,124 @@ const bcrypt = require("bcryptjs");
 
 const Event = require("../../models/event");
 const User = require("../../models/user");
+const Booking = require("../../models/bookings");
+// const { populate } = require("../../models/event");
 
 module.exports = {
-  events: () => {
-    return Event.find()
-      .populate({
-        path: "creator",
-        populate: { path: "createdEvents" },
-      })
-      .then((events) => {
-        return events.map((event) => {
-          return {
-            ...event._doc,
-            creator: { ...event._doc.creator._doc, password: null },
-          };
-        });
-      })
-      .catch((err) => {
-        throw err;
+  events: async () => {
+    try {
+      const events = await Event.find().populate("creator");
+      //   old chain:
+      //   .populate({
+      //     path: "creator",
+      //     populate: { path: "createdEvents" },
+      //   });
+      return events.map((event) => {
+        return {
+          ...event._doc,
+          creator: { ...event._doc.creator._doc, password: null },
+        };
       });
+    } catch (error) {
+      throw error;
+    }
   },
-  createEvent: (args) => {
+  bookings: async () => {
+    try {
+      const bookings = await Booking.find().populate("user").populate("event");
+
+      return bookings.map((booking) => {
+        return {
+          ...booking._doc,
+          createdAt: new Date(booking._doc.createdAt).toISOString(),
+          updatedAt: new Date(booking._doc.updatedAt).toISOString(),
+        };
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+  createEvent: async (args) => {
     const event = new Event({
       title: args.eventInput.title,
       description: args.eventInput.description,
       price: +args.eventInput.price,
       date: new Date(args.eventInput.date),
-      creator: "62f2671949fac466244d3584",
+      creator: "62f4ce0322875a23250a7d51",
     });
     let createdEvent;
-    return event
-      .save()
-      .then((result) => {
-        createdEvent = { ...result._doc };
-        return User.findById("62f2671949fac466244d3584");
-        console.log(result);
-        return { ...result._doc };
-      })
-      .then((user) => {
-        if (!user) {
-          throw new Error("user not found");
-        }
-        user.createdEvents.push(event);
-        return user.save();
-      })
-      .then((result) => {
-        return createdEvent;
-      })
-      .catch((err) => {
-        console.log(err);
-        throw err;
-      });
+
+    try {
+      const result = await event.save();
+      const creator = await User.findById("62f4ce0322875a23250a7d51");
+
+      createdEvent = {
+        ...result._doc,
+        date: new Date(event._doc.date).toISOString(),
+        creator: creator,
+      };
+
+      if (!creator) {
+        throw new Error("user not found");
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+
+      return createdEvent;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   },
 
-  createUser: (args) => {
-    return User.findOne({ email: args.userInput.email })
-      .then((user) => {
-        if (user) {
-          throw new Error("user already exists");
-        }
-        return bcrypt.hash(args.userInput.password, 12);
-      })
-      .then((hashedPassword) => {
-        const user = new User({
-          email: args.userInput.email,
-          password: hashedPassword,
-        });
-        return user.save();
-      })
-      .then((result) => {
-        return { ...result._doc, password: null };
-      })
-      .catch((err) => {
-        throw err;
+  createUser: async (args) => {
+    try {
+      const existingUser = await User.findOne({ email: args.userInput.email });
+      if (existingUser) {
+        throw new Error("user already exists");
+      }
+      //   if user doesn't exist already, then ...
+      const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+
+      const user = new User({
+        email: args.userInput.email,
+        password: hashedPassword,
       });
+
+      const result = await user.save();
+
+      return {
+        ...result._doc,
+        password: null,
+      };
+    } catch (error) {
+      throw error;
+    }
+  },
+  bookEvent: async (args) => {
+    const fetchedEvent = await Event.findOne({ _id: args.eventId });
+    const booking = new Booking({
+      user: "62f3b192f74add192117f8da",
+      event: fetchedEvent,
+    });
+    const result = await booking.save();
+    return {
+      ...result._doc,
+      createdAt: new Date(result._doc.createdAt).toISOString(),
+      updatedAt: new Date(result._doc.updatedAt).toISOString(),
+    };
+  },
+  cancelBooking: async (args) => {
+    try {
+      const booking = await Booking.findById(args.bookingId).populate("event");
+      const event = {
+        ...booking.event._doc,
+        creator: User.findById("62f3b192f74add192117f8da"),
+      };
+      await Booking.deleteOne({ _id: args.bookingId });
+      return event;
+    } catch (error) {
+      throw error;
+    }
   },
 };
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+### GraphQL backend API
+
+Based on tutorial by: [AcadeMind](https://www.youtube.com/watch?v=6y44oPKfqXU&list=PL55RiY5tL51rG1x02Yyj93iypUuHYXcB_&index=9)
